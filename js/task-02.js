ebf7aacd168edcd059100fcff509822687180c71
Oleg-Kaniuk <129388390+Oleg-Kaniuk@users.noreply.github.com>
2023-07-22 12:07:08 +0300

    add 2 task

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,21 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients')
+// console.dir(ingredientsList);
+
+const elements = ingredients.map(elem => {
+  const itemLi = document.createElement('li');
+  itemLi.textContent = elem;
+  itemLi.classList.add('item');
+  return itemLi;
+}); 
+
+ingredientsList.append(...elements);
+
+
+
+
+
+
