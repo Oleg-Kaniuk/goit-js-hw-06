1d173b4bb149ab801d69e2343dae83f9b9414f8c
Oleg-Kaniuk <129388390+Oleg-Kaniuk@users.noreply.github.com>
2023-07-24 10:53:52 +0300

    add 9 task

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,20 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+// const widget = document.querySelector('.widget')
+// console.dir(widget);
+const bgColor = document.querySelector('.color')
+// console.dir(bgColor);
+const changeColor = document.querySelector('.change-color')
+// console.dir(changeColor);
+const body = document.querySelector('body')
+// console.dir(body);
+
+const changeBackgroundColor = () => {
+  bgColor.textContent = getRandomHexColor()
+  body.style.backgroundColor = bgColor.textContent
+// body.style.backgroundColor = bgColor.textContent = getRandomHexColor()
+} 
+
+changeColor.addEventListener('click', changeBackgroundColor)
